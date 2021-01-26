8cc847ff7c8ac312ec729ab258d4bbcd99f06621
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-01-26 13:33:04 -0800

    make munge utility functions

--- results/results.js
+++ results/results.js
@@ -26,7 +26,7 @@ const ctx = document.getElementById('chart').getContext('2d');
 const data = [12, 19, 3, 5, 2, 3];
 const labelColors = ['red', 'blue', 'yellow', 'green', 'purple', 'orange'];
 
-const myChart = new Chart(ctx, {
+let myChart = new Chart(ctx, {
     type: 'bar',
     data: {
         labels: labelColors,
