c64a27a5a013d03100c389af6d2ae270532745f3
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-01-26 08:44:05 -0800

    debug render results function

--- results/render-results.js
+++ results/render-results.js
@@ -1,33 +1,23 @@
-// import stats and id function
-import { getPokeStats } from '../localStorage.js';
-import { pokemonData } from '../data.js';
-import { findByUnderScoreId } from '../utils.js';
 
 // make a render function
-export function renderResults() {
-    // pull in pokestats
-    const pokeStats = getPokeStats();
+export function renderResults(cartItem, pokeChar) {
 
-    // find matching ids in the pokestats by looping
-    for (let item of pokeStats) {
-        const pokeChar = findByUnderScoreId(pokemonData, item.id);
+    // render table elements - one row and three cells for results
+    const tr = document.createElement('tr');
 
-        // render table elements - one row and three cells for results
-        const tr = document.createElement('tr');
-        const pokemonTd = document.createElement('td');
-        const pokemonCaughtTd = document.createElement('td');
-        const pokemonSeenTd = document.createElement('td');
+    const pokemonTd = document.createElement('td');
+    const pokemonCaughtTd = document.createElement('td');
+    const pokemonSeenTd = document.createElement('td');
 
-        // replace their text content
-        pokemonTd.textContent = pokeChar.pokemon;
-        pokemonCaughtTd.textContent = item.caught;
-        pokemonSeenTd.textContent = item.seen;
+    // replace their text content
+    pokemonTd.textContent = pokeChar.pokemon;
+    pokemonCaughtTd.textContent = cartItem.caught;
+    pokemonSeenTd.textContent = cartItem.seen;
 
-        // append table
-        tr.append(pokemonTd, pokemonCaughtTd, pokemonSeenTd);
+    // append table
+    tr.append(pokemonTd, pokemonCaughtTd, pokemonSeenTd);
 
-        return tr;
-    }
+    return tr;
 }
 
 
---
+++ results/results.js
@@ -0,0 +1,16 @@
+import { getPokeStats } from '../localStorage.js';
+import { findByUnderScoreId } from '../utils.js';
+import { pokemonData } from '../data.js';
+import { renderResults } from './render-results.js';
+
+const table = document.getElementById('results-table');
+
+const resultsData = getPokeStats();
+
+for (let item of resultsData) {
+    const pokeChar = findByUnderScoreId(pokemonData, item.id);
+
+    let tr = renderResults(item, pokeChar);
+
+    table.append(tr);
+}
