68245e08a0cd470710f99a68d7af2618c8c516d8
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-01-26 13:17:30 -0800

    add chart template to html and results page

--- results/results.js
+++ results/results.js
@@ -20,3 +20,30 @@ newGameButton.addEventListener('click', () => {
     localStorage.removeItem('POKESTATS');
     window.location = '../';
 });
+
+const ctx = document.getElementById('chart').getContext('2d');
+
+const data = [12, 19, 3, 5, 2, 3];
+const labelColors = ['red', 'blue', 'yellow', 'green', 'purple', 'orange'];
+
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: labelColors,
+        datasets: [{
+            label: '# of Votes',
+            data: data,
+            backgroundColor: labelColors
+        }]
+    },
+    options: {
+        responsive: false,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
