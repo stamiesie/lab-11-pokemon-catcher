f04d8c6a9244a1636bd908f8ab3998fb1102bad3
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-01-26 13:58:42 -0800

    add munge utils to results chart

--- results/results.js
+++ results/results.js
@@ -2,6 +2,7 @@ import { getPokeStats } from '../localStorage.js';
 import { findByUnderScoreId } from '../utils.js';
 import { pokemonData } from '../data.js';
 import { renderResults } from './render-results.js';
+import { makeSeenArray, makeCaughtArray, makeLabelArray } from './mungeUtls.js';
 
 const table = document.getElementById('results-table');
 const newGameButton = document.getElementById('new-game-button');
@@ -21,20 +22,28 @@ newGameButton.addEventListener('click', () => {
     window.location = '../';
 });
 
-const ctx = document.getElementById('chart').getContext('2d');
-
-const data = [12, 19, 3, 5, 2, 3];
-const labelColors = ['red', 'blue', 'yellow', 'green', 'purple', 'orange'];
-
-let myChart = new Chart(ctx, {
+var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: labelColors,
-        datasets: [{
-            label: '# of Votes',
-            data: data,
-            backgroundColor: labelColors
-        }]
+        labels: makeLabelArray(resultsData),
+        datasets: [
+            {
+                label: '# of times seen',
+                data: makeSeenArray(resultsData),
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 1
+            },
+            {
+                label: '# of times caught',
+                data: makeCaughtArray(resultsData),
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 1
+            }
+
+        ]
     },
     options: {
         responsive: false,
