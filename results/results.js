bc5c2552e6811328002a43edf2f222860728efd8
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-01-26 11:34:32 -0800

    add play again button

--- results/results.js
+++ results/results.js
@@ -4,13 +4,19 @@ import { pokemonData } from '../data.js';
 import { renderResults } from './render-results.js';
 
 const table = document.getElementById('results-table');
+const newGameButton = document.getElementById('new-game-button');
 
 const resultsData = getPokeStats();
 
 for (let item of resultsData) {
     const pokeChar = findByUnderScoreId(pokemonData, item._id);
-    console.log(pokeChar);
+
     let tr = renderResults(item, pokeChar);
 
     table.append(tr);
 }
+
+newGameButton.addEventListener('click', () => {
+    localStorage.removeItem('POKESTATS');
+    window.location = '../';
+});
